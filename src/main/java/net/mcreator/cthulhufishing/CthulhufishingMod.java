fec664a640196a00fd521e9be0c59b68742da5bb
Mijio <91673971+Mijio@users.noreply.github.com>
2024-02-05 14:53:06 +0300

    MCreator Merge commit '758a295058e4bbc11e3a2fd9785efe9c1b1926a9' into c2b305844fc32d8b7ad048263e0b86f008fbd4de

--- src/main/java/net/mcreator/cthulhufishing/CthulhufishingMod.java
+++ src/main/java/net/mcreator/cthulhufishing/CthulhufishingMod.java
@@ -33,7 +33,6 @@ import net.minecraft.network.FriendlyByteBuf;
 
 import net.mcreator.cthulhufishing.init.CthulhufishingModTabs;
 import net.mcreator.cthulhufishing.init.CthulhufishingModSounds;
-import net.mcreator.cthulhufishing.init.CthulhufishingModParticleTypes;
 import net.mcreator.cthulhufishing.init.CthulhufishingModMobEffects;
 import net.mcreator.cthulhufishing.init.CthulhufishingModMenus;
 import net.mcreator.cthulhufishing.init.CthulhufishingModItems;
@@ -71,7 +70,7 @@ public class CthulhufishingMod {
 		CthulhufishingModMobEffects.REGISTRY.register(bus);
 
 		CthulhufishingModEnchantments.REGISTRY.register(bus);
-		CthulhufishingModParticleTypes.REGISTRY.register(bus);
+
 		CthulhufishingModMenus.REGISTRY.register(bus);
 
 		GeckoLib.initialize();
