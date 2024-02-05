e842638bc0749f342a3d57cff75f692f2a4b1c00
QuantumTrolling <st097173@student.spbu.ru>
2024-02-05 14:53:31 +0300

    MCreator Merge commit 'fec664a640196a00fd521e9be0c59b68742da5bb' into d9af09bfb8332b908d8845bb400dfd3ec2bdbb6d

---
+++ src/main/java/net/mcreator/cthulhufishing/init/CthulhufishingModParticles.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.cthulhufishing.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.mcreator.cthulhufishing.client.particle.PortalTentacleParticleParticle;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class CthulhufishingModParticles {
+	@SubscribeEvent
+	public static void registerParticles(RegisterParticleProvidersEvent event) {
+		event.register(CthulhufishingModParticleTypes.PORTAL_TENTACLE_PARTICLE.get(), PortalTentacleParticleParticle::provider);
+	}
+}
