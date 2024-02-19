a17ecb94b9d289328f205aa2bc37530c4a2e17bd
QuantumTrolling <st097173@student.spbu.ru>
2024-02-19 18:54:12 +0300

    MCreator Merge commit '5c0e058b83c4b875cff4e04c9ad88b8b79ecaa66' into bb1fc64f0a742a505bcce29a681ead65173f6ad3

--- src/main/java/net/mcreator/cthulhufishing/init/CthulhufishingModSounds.java
+++ src/main/java/net/mcreator/cthulhufishing/init/CthulhufishingModSounds.java
@@ -30,6 +30,4 @@ public class CthulhufishingModSounds {
 	public static final RegistryObject<SoundEvent> DAMAGE_FOSSIL_FISH = REGISTRY.register("damage_fossil_fish", () -> new SoundEvent(new ResourceLocation("cthulhufishing", "damage_fossil_fish")));
 	public static final RegistryObject<SoundEvent> IDLE_FOSSIL_FISH = REGISTRY.register("idle_fossil_fish", () -> new SoundEvent(new ResourceLocation("cthulhufishing", "idle_fossil_fish")));
 	public static final RegistryObject<SoundEvent> FOSSIL_FISH_ATTACK = REGISTRY.register("fossil_fish_attack", () -> new SoundEvent(new ResourceLocation("cthulhufishing", "fossil_fish_attack")));
-	public static final RegistryObject<SoundEvent> JUMP_FOSSIL_FISH_ATTACK = REGISTRY.register("jump_fossil_fish_attack", () -> new SoundEvent(new ResourceLocation("cthulhufishing", "jump_fossil_fish_attack")));
-	public static final RegistryObject<SoundEvent> SUMMON_FOSSIL_FISH = REGISTRY.register("summon_fossil_fish", () -> new SoundEvent(new ResourceLocation("cthulhufishing", "summon_fossil_fish")));
 }
